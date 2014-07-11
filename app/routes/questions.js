7320b4bb7b24c535d6e57545bb50e7f5cc580df4
harish2rock <harish.bhavanichikar@perficient.com>
2014-07-11 09:46:32 -0400

    adding radio options

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -1,17 +1,60 @@
 export default Ember.Route.extend({
     model: function () {
-        return [{
+        return [
+    {
     id: "1",
-    type: "1",
+    textarea: "yes",
+    radio: null,
+    options: "",
     title: "CSS",
     body: "Write CSS classes that will remove the rounded corners from the Earth.",
     answer:""
-}, {
+},
+{
     id: "2",
-    type: "1",
+    textarea: null,
+    radio: "yes",
+    options: { value1: "css", value2: "html", value3: "javascript", value4: "xml" },
     title: "Javascript",
-    body: "Write a javascript function to create the perfect cup of tea.",
+    body: "Select your favorite language",
     answer: ""
-}];
+},
+ {
+    id: "3",
+    textarea: "yes",
+    radio: null,
+    options: "",
+    title: "CSS",
+    body: "Write CSS classes that will remove the rounded corners from the Earth.",
+    answer:""
+},
+{
+    id: "4",
+    textarea: null,
+    radio: "yes",
+    options: { value1: "css", value2: "html", value3: "javascript", value4: "xml" },
+    title: "Javascript",
+    body: "Select your favorite language",
+    answer: ""
+},
+ {
+    id: "5",
+    textarea: "yes",
+    radio: null,
+    options: "",
+    title: "CSS",
+    body: "Write CSS classes that will remove the rounded corners from the Earth.",
+    answer:""
+},
+{
+    id: "6",
+    textarea: null,
+    radio: "yes",
+    options: { value1: "css", value2: "html", value3: "javascript", value4: "xml" },
+    title: "Javascript",
+    body: "Select your favorite language",
+    answer: ""
+}
+];
     }
 });
