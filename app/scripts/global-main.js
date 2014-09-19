302b9e7379adc3ff688d6dfdecb7d49718918ea2
harish2rock <harish.bhavanichikar@perficient.com>
2014-09-19 09:24:13 -0500

    mail script changes

--- app/scripts/global-main.js
+++ app/scripts/global-main.js
@@ -21,47 +21,6 @@ Ember.View.reopen({
                 });
             });
 
-
-
-
-
-         //    var name;
-         //    var radioValue;
-         //    var radioOption;
-
-
-
-         //   $('.radios').on('click',function() {
-         //      name = $(this).next('input').attr('id');
-         //     radioValue = $('#'+name).val();
-         //     });
-         // //     $('input[type=radio]').each(function() {
-
-         //    $('input[name='+name+']').each(function() {
-
-         //          radioOption = $(this).val();
-         //         alert(radioOption);
-         //        //  if(radioValue == radioOption) {
-         //        //     $("input[value="+radioOption+"]").prop("checked", true);
-         //        // }
-         //    });
-
-
-         //    });
-
-
-//           $("form").bind("DOMSubtreeModified", function() {
-//              $('input[type=radio]').each(function() {
-//       var radioOption = $(this).val();
-//     var name = this.name;
-//     var radioValue = $('#'+name).val();
-//      $("input[name="+name+"]").prop("checked", false);
-//     if(radioValue == radioOption) {
-//         $("input[value="+radioOption+"]").prop("checked", true);
-//     }
-//     });
-// });
-
             $('#btnStart').click(function () {
 
                 if ($('#chkAgree').is(':checked')) {
@@ -87,52 +46,6 @@ Ember.View.reopen({
 
         });
 
-        // var timer = $('#timer');
-        // var start_time = $.now();
-        // var time_limit_in_minutes = 0.25;
-        // var end_time = (start_time + (time_limit_in_minutes * 60 * 1000));
-        // var hashed_end_time = CryptoJS.MD5(end_time.toString());
-
-        // // timer scripts
-        // setInterval(function(){
-        //     var now = $.now();
-        //     var hashed_now = CryptoJS.MD5(now.toString());
-        //     // var result = (now >= end_time) ? "time's up!" : "not yet...";
-        //     // console.log("S: "+start_time+" / E: "+end_time+" / NOW: "+now);
-        //     // console.log(result);
-
-        //     if (hashed_now == hashed_end_time){
-        //         console.log("time's up!");
-        //     } else {
-        //         console.log("before or after");
-        //     }
-        // }, 1000);
-// $(document).ready(function(){
-// // 200 seconds countdown
-// var countdown = 10;
-
-// //current timestamp
-// var now   = Date.parse(new Date());
-
-// //ready should be stored in your cookie
-// var ready = Date.parse(new Date (now + countdown  * 1000)); // * 1000 to get ms
-
-
-// //every 1000 ms
-// setInterval(function()
-// {
-//     $.cookie('timervalue', ready, {expires: 1});
-
-// var cookieValue = $.cookie('timervalue');
-//     var sec = ( cookieValue - Date.parse(new Date()) )/1000;
-//     document.title = sec + " seconds left";
-
-
-
-// },1000);
-// });
-
-
     },
     didInsertElement: function(){
              $(document).ready(function() {
@@ -142,6 +55,24 @@ Ember.View.reopen({
   $('#'+a).removeClass('hidden');
 
 });
+
+  //alert('test');
+  $('#submit').on('click',function(e){
+    //e.preventDefault();
+    var formData = $("#questionnaire_form").serializeArray();
+    console.log(formData);
+    $.post("send_mail.php", formData)
+      .done(function(data){
+        console.log("post succeeded!")
+        // redirect to thank you page here
+      })
+      .fail(function(data){
+        console.log("failed to post")
+      })
+      .always(function(data){
+      })
+  });
+
 
 
         });
